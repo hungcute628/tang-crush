6f893bf2adb22948984b3a371cc2994c7634761a
hungcute628 <85265856+hungcute628@users.noreply.github.com>
2021-06-03 08:48:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -11,6 +11,6 @@ const CONFIG = {
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    btnAccept: 'Okiiiii lun <333',
     messLink: 'http://fb.com/27042003s' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
